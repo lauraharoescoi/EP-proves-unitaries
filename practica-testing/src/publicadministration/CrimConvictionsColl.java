c0430ec73e28f2e25623944e0f0a6d524d556207
lauraharoescoi <lauraha.escoi@gmail.com>
2022-12-31 12:11:46 +0100

    setup ready

---
+++ practica-testing/src/publicadministration/CrimConvictionsColl.java
@@ -0,0 +1,20 @@
+package publicadministration;
+
+import publicadministration.CrimConviction;
+
+import java.util.Date;
+
+public class CrimConvictionsColl {
+
+    public CrimConvictionsColl (){ . . . }
+
+    public addCriminalConviction (CrimConviction crmC){ . . . }
+    // Adds a criminal conviction
+
+    public CrimConviction getCriminalConviction (Date date){ . . . }
+    // Gets a specific criminal conviction by date
+
+    public String toString () { . . . } // Converts to String
+
+}
+
