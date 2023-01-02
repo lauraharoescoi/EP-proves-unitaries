7cb4338d75a63e4e6595b4ebc35b8a870c3d028f
unknown <polescola3000@gmail.com>
2023-01-02 20:36:00 +0100

    services and dummies

--- practica-testing/src/services/JusticeMinistry.java
+++ practica-testing/src/services/JusticeMinistry.java
@@ -1,10 +1,15 @@
 package services;
 
 import data.Goal;
+import data.exceptions.BadPathException;
+import data.exceptions.DigitalSignatureException;
 import publicadministration.Citizen;
+import exceptions.*;
+import publicadministration.CriminalRecordCertf;
 
 public interface JusticeMinistry {
 
-    CriminalRecordCertf getCriminalRecordCertf (Citizen persD, Goal g) throws DigitalSignatureException,ConnectException7;
+    CriminalRecordCertf getCriminalRecordCertf (Citizen persD, Goal g) throws DigitalSignatureException, ConnectException, BadPathException;
 
 }
+
